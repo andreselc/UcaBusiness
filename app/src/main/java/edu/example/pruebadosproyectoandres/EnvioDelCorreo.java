6f14151a65f09df67a7480f268fe3dd4b9b5f760
kat <68612712+blondiedies@users.noreply.github.com>
2022-02-03 02:39:26 -0400

    CLASE DE EnviarCorreo. Explicación en el MainActivity. Contraseña olvidada está listo.

--- app/src/main/java/edu/example/pruebadosproyectoandres/EnvioDelCorreo.java
+++ app/src/main/java/edu/example/pruebadosproyectoandres/EnvioDelCorreo.java
@@ -24,6 +24,7 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -95,8 +96,6 @@ public class EnvioDelCorreo extends Activity implements View.OnClickListener {
                 message.setContent(textMessage, "text/html; charset=utf-8");
                 Transport.send(message);
             } catch (MessagingException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
